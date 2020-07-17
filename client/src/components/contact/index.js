dac147ecc8220a825d52dde691242d84ba15cd0b
EDowning2000 <emersondowning@gmail.com>
2020-07-17 17:51:42 -0400

    all pages have their html in them but need more interactive and uninteractive content:

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -6,16 +6,16 @@ function contact() {
   render(
     <div className="firstDiv">
       <input id="anPageName" name="page" type="hidden" value="contact" />
-      <div class="contact anima-word-break ">
+      <div className="contact anima-word-break ">
         <div className="secondDiv">
           <img
             alt="Team Pros at Work Logo"
             anima-src="./img/landing-page-ferlan-and-natalia-png@2x.png"
-            class="ferlanandnataliapng"
+            className="ferlanandnataliapng"
             src={logo}
           />
-          <div class="a2020byemersondown">2020 by emerson downing</div>
-          <div class="interestedinworkin">Interested in working with us?</div>
+          <div className="a2020byemersondown">2020 by emerson downing</div>
+          <div className="interestedinworkin">Interested in working with us?</div>
         </div>
       </div>
     </div>
--- client/src/components/pros/index.js
+++ client/src/components/pros/index.js
@@ -3,6 +3,21 @@ import "./index.scss";
 import logo from "../../../images/ferlanandnataliapng.png";
 
 function pros() {
-  render();
+  render(
+    <div className="firstDiv">
+      <input id="anPageName" name="page" type="hidden" value="thepros" />
+      <div className="thepros anima-word-break ">
+        <div className="secondDiv">
+          <img
+            alt="Team Pros at Work Image"
+            anima-src="./img/landing-page-ferlan-and-natalia-png@2x.png"
+            className="ferlanandnataliapng"
+            src={logo}
+          />
+          <div className="a2020byemersondown">2020 by emerson downing</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default pros;
