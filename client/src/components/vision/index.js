86fcf0117ceffced3290967ab25be025b52e6952
EDowning2000 <emersondowning@gmail.com>
2020-08-02 18:46:25 -0400

    vision page is finished and formatted

--- client/src/components/vision/index.js
+++ client/src/components/vision/index.js
@@ -1,24 +1,37 @@
-import React from 'react'
-import './index.scss'
+import React from "react";
+import "./index.scss";
 
-function vision(){
+function vision() {
   return (
-    <div className='container'>
+    <div className="container">
       <div className="theVision">The Vision</div>
-      <div classname="information">In a world where you have all the information at your fingertips, its nothing short of a chore to decifer what is good information, and what isnt. Many people go their entire fitness journeys without any good information, jumping from diet to diet, with no accountability. This can seem daunting, and almost as if you will never get to your end goal. Thats where the Pro’s come in.
-- </div>
-      <div classname="joining">Upon joining, you will recieve detailed nutritional plans that will change according to your bodys needs, workout plans and information on how to work through or around injuries, constant reassurance and motivation, and 24/7 support for any questions you may have. </div>
-      <div classname="monitoring">As a member of our team, you will constantly be monitored, checked in on, and supported throughout your fitness journey.</div>
-      <div classname="types">Whether that be contest prep, off season bulking, losing weight, or even just getting health markers in better places, we have got you covered.
--</div>
-      <div classname="whatWeOffer">From the start, our mission is to see our clients succeed, meet their potential, learn good habits, and be healthier individuals along the way.
--   </div>
-
-
-
-
-
-
+      <div classname="information">
+        In a world where you have all the information at your fingertips, its
+        nothing short of a chore to decifer what is good information, and what
+        isnt. Many people go their entire fitness journeys without any good
+        information, jumping from diet to diet, with no accountability. This can
+        seem daunting, and almost as if you will never get to your end goal.
+        Thats where the Pro’s come in. -{" "}
+      </div>
+      <div classname="joining">
+        Upon joining, you will recieve detailed nutritional plans that will
+        change according to your bodys needs, workout plans and information on
+        how to work through or around injuries, constant reassurance and
+        motivation, and 24/7 support for any questions you may have.{" "}
+      </div>
+      <div classname="monitoring">
+        As a member of our team, you will constantly be monitored, checked in
+        on, and supported throughout your fitness journey.
+      </div>
+      <div classname="types">
+        Whether that be contest prep, off season bulking, losing weight, or even
+        just getting health markers in better places, we have got you covered. -
+      </div>
+      <div classname="whatWeOffer">
+        From the start, our mission is to see our clients succeed, meet their
+        potential, learn good habits, and be healthier individuals along the
+        way. -{" "}
+      </div>
     </div>
-  )
+  );
 }
