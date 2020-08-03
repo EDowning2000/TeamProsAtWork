f02df66c9f7a1dc13abf22ae6e5eb685c472d65e
EDowning2000 <emersondowning@gmail.com>
2020-08-03 17:24:50 -0400

    vision page is finished and tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Template from './components/template/index'
+import Vision from './components/vision/index'
 
 function App() {
   return (
-  <Template></Template>
+  // <Template></Template>
+  <Vision></Vision>
   );
 }
 
--- client/src/components/vision/index.js
+++ client/src/components/vision/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
-import './index.css';
-import Template from '../template/index'
+import React from "react";
+import "./index.css";
+import Template from "../template/index";
 
-export default function Vision(){
+export default function Vision() {
   return (
-    <Template>
-      
-
-
-
-
-
-    </Template>
-  )
+    <div className="visionContainer">
+      <Template></Template>
+      <div className="theVision">The Vision</div>
+      <div className="vision1">
+        From the start, our mission is to see our clients succeed, meet their
+        potential, learn good habits, and be healthier individuals along the
+        way. -
+      </div>
+      <div className="vision2">
+        In a world where you have all the information at your fingertips, its
+        nothing short of a chore to decifer what is good information, and what
+        isnt. Many people go their entire fitness journeys without any good
+        information, jumping from diet to diet, with no accountability. This can
+        seem daunting, and almost as if you will never get to your end goal.
+        Thats where the Pro’s come in. -
+      </div>
+      <div className="vision3">
+        As a member of our team, you will constantly be monitored, checked in
+        on, and supported throughout your fitness journey. -
+      </div>
+      <div className="vision4">
+        Whether that be contest prep, off season bulking, losing weight, or even
+        just getting health markers in better places, we have got you covered. -
+      </div>
+      <div className="vision5">
+        Upon joining, you will recieve detailed nutritional plans that will
+        change according to your bodys needs, workout plans and information on
+        how to work through or around injuries, constant reassurance and
+        motivation, and 24/7 support for any questions you may have.
+      </div>
+    </div>
+  );
 }
