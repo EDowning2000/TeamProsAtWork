44fb5899d6e84e90cf43939f45d04d48b3ac94a3
EDowning2000 <emersondowning@gmail.com>
2020-08-03 18:25:39 -0400

    homepage is finished but needs a photo with some halfway decent quality still

---
+++ client/src/components/home/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+import Template from '../template/index';
+import natPic from '../../images/'
+
+export default function Home(){
+  return (
+    <div className='homeContainer'>
+      <Template></Template>
+      <img src={natPic} alt="Natalia at Olympia" className='natPic'></img>
+      <div className="homeMotto">A big part of our lives is eating, recovering, and going to the gym. But even IFBB Pro’s have to work. Get all your information from the people who not only do this for a living, but also who have made it, The Pro’s. </div>
+      <div className='teamProsAtWorkHome'>Team Pro’s At Work</div>
+      <div className="contestPrep">Contest Prep And Lifestyle Coaching</div>
+      <div className="homeRectangle1"></div>
+      <div className="homeRectangle2"></div>
+      
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
