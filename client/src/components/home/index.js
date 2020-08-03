02175790182a221d1d1e62767f3b1877a56fd59c
EDowning2000 <emersondowning@gmail.com>
2020-08-03 18:42:19 -0400

    homepage is finished with a different photo

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,27 +1,22 @@
-import React from 'react';
-import './index.css';
-import Template from '../template/index';
-import natPic from '../../images/'
+import React from "react";
+import "./index.css";
+import Template from "../template/index";
+import natPic from "../../images/natPic.jpg";
 
-export default function Home(){
+export default function Home() {
   return (
-    <div className='homeContainer'>
+    <div className="homeContainer">
       <Template></Template>
-      <img src={natPic} alt="Natalia at Olympia" className='natPic'></img>
-      <div className="homeMotto">A big part of our lives is eating, recovering, and going to the gym. But even IFBB Pro’s have to work. Get all your information from the people who not only do this for a living, but also who have made it, The Pro’s. </div>
-      <div className='teamProsAtWorkHome'>Team Pro’s At Work</div>
+      <img src={natPic} alt="Natalia at Olympia" className="natPic"></img>
+      <div className="homeMotto">
+        A big part of our lives is eating, recovering, and going to the gym. But
+        even IFBB Pro’s have to work. Get all your information from the people
+        who not only do this for a living, but also who have made it, The Pro’s.{" "}
+      </div>
+      <div className="teamProsAtWorkHome">Team Pro’s At Work</div>
       <div className="contestPrep">Contest Prep And Lifestyle Coaching</div>
       <div className="homeRectangle1"></div>
       <div className="homeRectangle2"></div>
-      
-
-
-
-
-
-
-
-
     </div>
-  )
+  );
 }
