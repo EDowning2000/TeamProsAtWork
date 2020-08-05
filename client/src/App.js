8efe147a9be2983f7682e2a0b95c6382fd82b809
EDowning2000 <emersondowning@gmail.com>
2020-08-05 13:21:09 -0400

    contact page is styled correctly

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-import Template from './components/template/index'
-import Vision from './components/vision/index'
+import React from "react";
+import "./App.css";
+import Template from "./components/template/index";
+import Vision from "./components/vision/index";
+import Contact from "./components/contact/index";
 
 function App() {
   return (
-  // <Template></Template>
-  <Vision></Vision>
+    // <Template></Template>
+    // <Vision></Vision>
+    <Contact></Contact>
   );
 }
 
