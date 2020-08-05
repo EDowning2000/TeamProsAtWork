21ade54d33fae324a43ea1655f5dc467953b23f7
EDowning2000 <emersondowning@gmail.com>
2020-08-05 10:50:45 -0400

    contact page has begun

---
+++ client/src/components/contact/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+import Template from '../template/index';
+
+export default function Contact (){
+  return(
+    <div className='contactContainer'>
+      <Template></Template>
+        <div className="interested">Interested in Working With Us?</div>
+    </div>
+  )
+}
