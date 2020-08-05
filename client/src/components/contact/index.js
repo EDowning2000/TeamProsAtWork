7f8dc5957b0957482a54f6aaf3d8538991d3b44d
EDowning2000 <emersondowning@gmail.com>
2020-08-05 11:34:05 -0400

    contact is getting there

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './index.css';
-import Template from '../template/index';
+import React from "react";
+import "./index.css";
+import Template from "../template/index";
 
-export default function Contact (){
-  return(
-    <div className='contactContainer'>
+export default function Contact() {
+  return (
+    <div className="contactContainer">
       <Template></Template>
-        <div className="interested">Interested in Working With Us?</div>
+      <div className="interested">Interested in Working With Us?</div>
+      <div className='followUs'>Follow Us on Instagram!</div>
+      <div className='emailUs'>Send Us an Email!</div>
+      <div className='workWithUs'></div>
     </div>
-  )
+  );
 }
