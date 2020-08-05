41aded4e7e0528612ceef9099cc39776e714c046
EDowning2000 <emersondowning@gmail.com>
2020-08-05 11:58:29 -0400

    contact pages images have been imported as well as the text has been applied to the files

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 import "./index.css";
 import Template from "../template/index";
+import iphone from "../../images/iphonePic.png";
+import instagramPic from "../../images/instagramLogo.png";
+import gmailPic from "../../images/gmailLogo.png";
 
 export default function Contact() {
   return (
     <div className="contactContainer">
       <Template></Template>
       <div className="interested">Interested in Working With Us?</div>
-      <div className='followUs'>Follow Us on Instagram!</div>
-      <div className='emailUs'>Send Us an Email!</div>
-      <div className='workWithUs'></div>
+      <div className="followUs">Follow Us on Instagram!</div>
+      <div className="emailUs">Send Us an Email!</div>
+      <div className="workWithUs">
+        If you are interested in working with us, reach out and we will begin
+        the process of adding you to the Team Pro’s At Work Family!
+      </div>
+      <img
+        src="instagramPic"
+        alt="instagram logo"
+        className="instagramPic"
+      ></img>
+      <img src="gmailPic" alt="gmail logo" className="gmailLogo"></img>
     </div>
   );
 }
