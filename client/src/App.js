d48271b7b4eac4effcc7f7e92a5948148b518b2a
EDowning2000 <emersondowning@gmail.com>
2020-08-06 19:46:47 -0400

    routes somehow fucked up the images on the home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Template from "./components/template/index";
 import Vision from "./components/vision/index";
 import Contact from "./components/contact/index";
 import Home from './components/home/index';
-import Pros from './components/pros/index';
+// import Pros from './components/pros/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -13,14 +13,14 @@ function App() {
       <Switch>
         <Route path ='/' exact component = {Home}/>
         <Route path ='/vision' exact component = {Vision}/>
-        <Route path ='/pros' exact component = {Pros}/>
+        {/* <Route path ='/pros' exact component = {Pros}/> */}
         <Route path ='/contact' exact component ={Contact}/>
 
       </Switch>
     </Router>
     // <Template></Template>
     // <Vision></Vision>
-    <Contact></Contact>
+    // <Contact></Contact>
   );
 }
 
