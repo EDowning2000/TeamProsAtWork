3e31afa975740a47b43370e1359c028782642222
EDowning2000 <emersondowning@gmail.com>
2020-08-06 17:17:55 -0400

    added the routing to the index file of the template but not to the app.js file yet

--- client/src/components/template/index.js
+++ client/src/components/template/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import "./index.css";
 import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Template() {
   return (
     <div className="mainContainer">
       <img src={logo} className="mainLogo" alt="main logo"></img>
-      <div className="vision">Vision</div>
+      <Link to="/vision">
+        <div className="vision">Vision</div>
+      </Link>
+      <Link to="/pros"></Link>
       <div className="pros">The Pro's</div>
-      <div className="contact">Contact</div>
-      <a className="emer" href="https://github.com/EDowning2000">Made with 🖤 by Emerson D.</a>
+      <Link to="/contact">
+        <div className="contact">Contact</div>
+      </Link>
+
+      <a className="emer" href="https://github.com/EDowning2000">
+        Made with 🖤 by Emerson D.
+      </a>
     </div>
   );
 }
