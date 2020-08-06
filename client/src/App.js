1c653b328213da532975e4d4762494519b40a7fc
EDowning2000 <emersondowning@gmail.com>
2020-08-06 17:23:39 -0400

    routes are in but not tested quite yet

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,21 @@ import "./App.css";
 import Template from "./components/template/index";
 import Vision from "./components/vision/index";
 import Contact from "./components/contact/index";
+import Home from './components/home/index';
+import Pros from './components/pros/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
+      <Switch>
+        <Route path ='/' exact component = {Home}/>
+        <Route path ='/vision' exact component = {Vision}/>
+        <Route path ='/pros' exact component = {Pros}/>
+        <Route path ='/contact' exact component ={Contact}/>
+
+      </Switch>
+    </Router>
     // <Template></Template>
     // <Vision></Vision>
     <Contact></Contact>
