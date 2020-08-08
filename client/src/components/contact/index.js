1855891adc394233aabc4e6a92ca1ffd57779150
EDowning2000 <emersondowning@gmail.com>
2020-08-08 12:37:48 -0400

    working on copy to clipboard

--- client/src/components/contact/index.js
+++ client/src/components/contact/index.js
@@ -1,37 +1,52 @@
 import React from "react";
+import ReactDom from "react-dom";
 import "./index.css";
 import Template from "../template/index";
 // import iphone from "../../images/iphonePic.png";
 //not currently needed but may add in the future for an instagram carousel
 import instagramLogo from "../../images/instagramLogo.png";
 import gmailLogo from "../../images/gmailLogo.png";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+// import { CopyToClipboard } from "react-copy-to-clipboard";
+import Clipboard from "react-clipboard.js";
 
+const emailAddress = "ifbbprosatwork@gmail.com";
 
-export default function Contact() {
-  const emailAddress = "ifbbprosatwork@gmail.com"
-  
-  return (
-    <div className="contactContainer">
-      <Template></Template>
-      <div className="interested">Interested in Working With Us?</div>
-      <a href="https://www.instagram.com/teamprosatwork/" target="blank">
-        <div className="followUs">Click to Follow Us on Instagram!</div>
-      </a>
-      <div className="emailUs">Click to Copy Our Email!</div>
-      <div className="workWithUs">
-        If you are interested in working with us, reach out and we will begin
-        the process of adding you to the Team Pro’s At Work Family!
+class Contact extends React.Component {
+  state = {
+    value: emailAddress,
+    copied: false,
+  };
+
+  render() {
+    return (
+      <div className="contactContainer">
+        <Template></Template>
+        <div className="interested">Interested in Working With Us?</div>
+        <a href="https://www.instagram.com/teamprosatwork/" target="blank">
+          <div className="followUs">Click to Follow Us on Instagram!</div>
+        </a>
+
+        <Clipboard data-clipboard-text={this.state.value}>
+          <div className="emailUs" onClick="copy">
+            Click to Copy Our Email!
+          </div>
+        </Clipboard>
+
+        <div className="workWithUs">
+          If you are interested in working with us, reach out and we will begin
+          the process of adding you to the Team Pro’s At Work Family!
+        </div>
+        {/* add an a tag and wrap all of the image in it. dont forget target = blank. then work on copy to clipboard */}
+        <a href="https://www.instagram.com/teamprosatwork/" target="blank">
+          <img
+            src={instagramLogo}
+            alt="instagram logo"
+            className="instagramlogo"
+          ></img>
+        </a>
+        <img src={gmailLogo} alt="gmail logo" className="gmailLogo"></img>
       </div>
-      {/* add an a tag and wrap all of the image in it. dont forget target = blank. then work on copy to clipboard */}
-      <a href="https://www.instagram.com/teamprosatwork/" target="blank">
-        <img
-          src={instagramLogo}
-          alt="instagram logo"
-          className="instagramlogo"
-        ></img>
-      </a>
-      <img src={gmailLogo} alt="gmail logo" className="gmailLogo"></img>
-    </div>
-  );
+    );
+  }
 }
+export default Contact;
