e5d654a316157a942495fee9a9aa7e83af35ff0c
EDowning2000 <emersondowning@gmail.com>
2020-08-09 21:49:12 -0400

    signing off, need to get info about ferlan and style the pros page. Also this is my last commit before my birthday tomorrowgit add -A ill have an amazing time for anyone reading this

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Template from "./components/template/index";
 import Vision from "./components/vision/index";
 import Contact from "./components/contact/index";
 import Home from './components/home/index';
-// import Pros from './components/pros/index';
+import Pros from './components/pros/index';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route path ='/' exact component = {Home}/>
         <Route path ='/vision' exact component = {Vision}/>
-        {/* <Route path ='/pros' exact component = {Pros}/> */}
+        <Route path ='/pros' exact component = {Pros}/>
         <Route path ='/contact' exact component ={Contact}/>
 
       </Switch>
---
+++ client/src/components/pros/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.css";
+import Template from "../template/index";
+
+class Pros extends React.Component {
+  render() {
+    return (
+      <div className="prosContainer">
+        <Template></Template>
+      <div className="description">
+        Before you begin working with us, we want to introduce ourselves! I am Natalia Coleho, I'm 24 years old and was raised in Rio de Janeiro, Brazil. I currently reside in Orlando Florida, and am currently working on my masters Degree at the University of Central Florida, along with this I also have my Bachelors Degree from UCF in Sports and Exercise Science. I started training at the age of 15, and have been completely obsessed since! I turned Pro at the age of 19, the youngest age ever, and since then have accumulated 12 first place victories. The most notible of these being 2 Arnold Classic victories. Along with this, i have also participated in 5 Olympia competitions! I am excited to bring the same dedication that allowed me 2 Arnold classic victories to you and your physique!
+      </div>
+
+
+
+      </div>
+    );
+  }
+}
+export default Pros;
