8134ebfa21e9b83e1831df51b0b17739ea1e7f1a
EDowning2000 <emersondowning@gmail.com>
2020-08-18 17:39:48 -0400

    got the bio's done

--- client/src/components/pros/index.js
+++ client/src/components/pros/index.js
@@ -7,9 +7,12 @@ class Pros extends React.Component {
     return (
       <div className="prosContainer">
         <Template></Template>
-        <div className="description">
-          Before you begin working with us, we want to introduce ourselves! I am
-          Natalia Coleho(Left), I'm 24 years old and was raised in Rio de
+        <div className="meetUs">Meet The Pro's</div>
+        <div className="description1">
+          Before you begin working with us, we want to introduce ourselves!
+        </div>
+        <div className="description2">
+          I am Natalia Coleho (Left), I'm 24 years old and was raised in Rio de
           Janeiro, Brazil. I currently reside in Orlando Florida, and am
           currently working on my masters Degree at the University of Central
           Florida, along with this I also have my Bachelors Degree from UCF in
@@ -19,15 +22,17 @@ class Pros extends React.Component {
           victories. The most notible of these being 2 Arnold Classic victories.
           Along with this, i have also participated in 5 Olympia competitions! I
           am excited to bring the same dedication that allowed me 2 Arnold
-          classic victories to you and your physique! And I am Ferlan
-          Bailey(Right)! Im originally from the Island of Grenada, but since the
-          age of 13 I have been living in the Orlando Area! Before becoming an
-          IFBB Pro, i worked as a Firefighter in Eustis, however now my time is
-          fully devoted to Coaching, being home with family, and running my
-          supplement brand. I turned pro in 2016 and have competed in numerous
-          shows since then. Although i dont have plans of competing anytime
-          soon, my love for bodybuilding is still a huge part of my life thanks
-          to coaching.
+          classic victories to you and your physique!
+        </div>
+        <div className="description3">
+          And I am Ferlan Bailey (Right)! Im originally from the Island of
+          Grenada, but since the age of 13 I have been living in the Orlando
+          Area! Before becoming an IFBB Pro, i worked as a Firefighter in
+          Eustis, however now my time is fully devoted to Coaching, being home
+          with family, and running my supplement brand. I turned pro in 2016 and
+          have competed in numerous shows since then. Although i dont have plans
+          of competing anytime soon, my love for bodybuilding is still a huge
+          part of my life thanks to coaching.
         </div>
       </div>
     );
